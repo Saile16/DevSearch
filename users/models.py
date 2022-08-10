1b68807d47c6bb03f004778c491b22ce99fe2c65
Alexander Lucano <alrlucano@gmail.com>
2022-08-10 14:06:55 -0500

    Usamos signals, revisar

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+
+
+
+
+
 # Create your models here.
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
@@ -21,7 +26,7 @@ class Profile(models.Model):
     id=models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
@@ -32,3 +37,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+    
---
+++ users/signals.py
@@ -0,0 +1,44 @@
+# ----USANDO SIGNALS ----
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
+
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+# @receiver(post_save,sender=Profile)
+# def profileUpdated(sender,instance,created,**kwargs):
+#     print('Profile saved!!')
+#     print('Instance:',instance)
+#     print('Created:',created)
+#     print('Kwargs:',kwargs)
+#     print('Sender',sender)
+
+# def deleteUser(sender,instance,**kwargs):
+#     print('User deleted!!')
+#     print('Instance:',instance)
+#     print('Kwargs:',kwargs)
+#     print('Sender',sender)
+
+# post_save.connect(profileUpdated,sender=Profile)
+# post_delete.connect(deleteUser,sender=Profile)
+
+#de esta manera estamos usando los signals para un profile al momento de crear el user
+def createProfile(sender,instance,created,**kwargs):
+    print('Profile signla trigerred')
+    if created:
+        user=instance
+        profile=Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+def deleteProfile(sender,instance,**kwargs):
+    print('Profile deleted signal triggered')
+    user=instance.user
+    user.delete()
+
+post_save.connect(createProfile,sender=User)
+post_delete.connect(deleteProfile,sender=Profile)
